a9725f5a36c23b8f51ea977de18d6a6de7ed633e
P Rishanth <mathematicsacademy007@gmail.com>
2026-07-15 08:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///keys.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class APIKey(db.Model):
+class APIKey(db.Model)
     id = db.Column(db.Integer, primary_key=True)
     key = db.Column(db.String(36), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
