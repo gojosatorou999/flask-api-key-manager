239aae565d3eb6ca4c9b8ed7a509524bd290a258
P Rishanth <mathematicsacademy007@gmail.com>
2026-09-20 03:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import uuid
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-    
+     
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///keys.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
